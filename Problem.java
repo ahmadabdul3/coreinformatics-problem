d840531ad6d01ba34959f83c7df8ee044695f577
Abdul Ahmad <abdulahmad@computer100.local>
2016-04-20 16:13:01 -0400

    added problem before refactoring

--- Problem.java
+++ Problem.java
@@ -13,4 +13,55 @@ public class Problem {
     }
     
     
+    /* before refactor*/
+    
+    /*
+
+       if(age > 18) {
+	        if(gender.equalsIgnoreCase("male")){
+	          if(age < 50) {
+	            if(heightUnit.equalsIgnoreCase("ft")) {
+	              if(height < 5 || height > 6.5) {
+	                return false;
+	              }else {
+	                return true;
+	              }
+	            }else if(heightUnit.equalsIgnoreCase("m")) {
+	              if(height < 1.524 || height > 1.981){
+	                return false;
+	              }else {
+	                return true;
+	              }
+	            }
+	          }else {
+	            return false;
+	          }
+	        }else if(gender.equalsIgnoreCase("female")) {
+	          if(age < 60) {
+	            if(heightUnit.equalsIgnoreCase("ft")) {
+	              if(height < 4.5 || height > 6) {
+	                return false;
+	              }else {
+	                return true;
+	              }
+	            }else if(heightUnit.equalsIgnoreCase("m")) {
+	              if(height < 1.372 || height > 1.829){
+	                return false;
+	              }else {
+	                return true;
+	              }
+	            }
+	          }else {
+	            return false;
+	          }          
+	        }
+	      }else {
+	        return false;
+	      }
+	      return false;
+	    }
+
+     */
+    
+    
 }
